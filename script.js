a3d6b00b9726bb2054d8648aecd2a63a547059b8
derhudas <derhudas@proton.me>
2026-06-15 12:28:53 +0700

    koneksi vercel

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const SUPABASE_URL = "https://cyevvhoessdlplpiiwdc.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_M138Ywr1_yqE6B5i10yEZw_nUsCgwU1";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function testConnection() {
+
+    const { data, error } = await supabase
+        .from("tasks")
+        .select("*")
+        .limit(5);
+
+    if (error) {
+        console.error("Error:", error);
+        alert("Gagal mengambil data.");
+        return;
+    }
+
+    console.log(data);
+
+    alert(
+        `Berhasil mengambil ${data.length} tugas dari Supabase!`
+    );
+}
+
+testConnection();
+
 function addTask(button) {
     const column = button.parentElement;
 
