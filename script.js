d2c4128f41269e59caf5259714ddfcbbf0e58820
derhudas <derhudas@proton.me>
2026-06-15 17:47:10 +0700

    test fetchig data

--- script.js
+++ script.js
@@ -9,6 +9,39 @@ const supabase = window.supabase.createClient(
 
 async function loadTasks() {
 
+    try {
+        const supabase = window.supabase.createClient(
+            SUPABASE_URL,
+            SUPABASE_KEY
+        );
+
+        const { data, error } = await supabase
+            .from("tasks")
+            .select("*")
+            .limit(3);
+
+        if (error) {
+            document.body.innerHTML += `
+                <pre>Error:
+${JSON.stringify(error, null, 2)}
+                </pre>
+            `;
+            return;
+        }
+
+        document.body.innerHTML += `
+            <h2>Berhasil mengambil ${data.length} data!</h2>
+            <pre>${JSON.stringify(data, null, 2)}</pre>
+        `;
+
+    } catch (err) {
+        document.body.innerHTML += `
+            <pre>Exception:
+${err.message}
+            </pre>
+        `;
+    }
+
     const { data, error } = await supabase
         .from("tasks")
         .select("*")
