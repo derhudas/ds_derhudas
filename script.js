44d885bae9c8bfd33c23e7c5a296a8e88fedf2ae
derhudas <derhudas@proton.me>
2026-06-15 17:33:03 +0700

    test fetchig data

--- script.js
+++ script.js
@@ -2,41 +2,56 @@ const SUPABASE_URL = "https://cyevvhoessdlplpiiwdc.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_M138Ywr1_yqE6B5i10yEZw_nUsCgwU1";
 
-document.body.innerHTML += "<p>Mencoba terhubung...</p>";
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 
-async function testConnection() {
-    try {
-        const supabase = window.supabase.createClient(
-            SUPABASE_URL,
-            SUPABASE_KEY
-        );
+async function loadTasks() {
 
-        const { data, error } = await supabase
-            .from("tasks")
-            .select("*")
-            .limit(3);
+    const { data, error } = await supabase
+        .from("tasks")
+        .select("*")
+        .order("id");
 
-        if (error) {
-            document.body.innerHTML += `
-                <pre>Error:
-${JSON.stringify(error, null, 2)}
-                </pre>
-            `;
-            return;
+    if (error) {
+        document.body.innerHTML += `
+            <pre>${JSON.stringify(error, null, 2)}</pre>
+        `;
+        return;
+    }
+
+    const todoList = document.getElementById("todo-list");
+    const doingList = document.getElementById("doing-list");
+    const doneList = document.getElementById("done-list");
+
+    todoList.innerHTML = "";
+    doingList.innerHTML = "";
+    doneList.innerHTML = "";
+
+    data.forEach(task => {
+
+        const card = document.createElement("div");
+
+        card.className = "card";
+
+        card.innerHTML = `
+            <strong>${task.title}</strong><br>
+            Prioritas: ${task.priority}
+        `;
+
+        if (task.status === "todo") {
+            todoList.appendChild(card);
+        }
+        else if (task.status === "doing") {
+            doingList.appendChild(card);
+        }
+        else if (task.status === "done") {
+            doneList.appendChild(card);
         }
 
-        document.body.innerHTML += `
-            <h2>Berhasil mengambil ${data.length} data!</h2>
-            <pre>${JSON.stringify(data, null, 2)}</pre>
-        `;
+    });
 
-    } catch (err) {
-        document.body.innerHTML += `
-            <pre>Exception:
-${err.message}
-            </pre>
-        `;
-    }
 }
 
-testConnection();
+loadTasks();
